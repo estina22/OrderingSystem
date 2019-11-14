12816a0eac2ff103f5c1396649905beeb305ff7f
Estefany Lemus <37642009+estina22@users.noreply.github.com>
2019-11-14 14:40:55 -0600

    Add files via upload

---
+++ Final Project/OrderingSystem/src/orderingsystem/OrderingSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orderingsystem;
+
+/**
+ *
+ * @author eslemus
+ */
+
+//main driver
+public class OrderingSystem 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // initial window to start an order
+        new orderNow().setVisible(true);
+    }
+    
+}
---
+++ Final Project/OrderingSystem/src/orderingsystem/startOrder.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orderingsystem;
+import java.io.*;
+/**
+ *
+ * @author eslemus
+ */
+public class startOrder 
+{
+    private String location;
+
+    //constructor
+    public startOrder(String location) 
+    {
+        this.location = location;
+    }
+
+    //getter 
+    public String getLocation() 
+    {
+        return location;
+    }
+    
+    
+}
